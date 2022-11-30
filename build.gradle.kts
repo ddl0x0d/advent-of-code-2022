bb50817ccf7760e6bfd80faec23360afd37956d2
Dmitrijs Laptevs <dmitry.laptev.0x0d@gmail.com>
2022-11-30 22:07:41 +0200

    Configure unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
 plugins {
     kotlin("jvm") version "1.7.21"
     application
@@ -7,11 +9,23 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion: String by project
+
 dependencies {
     implementation(platform(kotlin("bom")))
     implementation(kotlin("stdlib"))
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 application {
     mainClass.set("aoc2022.AppKt")
+}
+
+tasks.test {
+    testLogging {
+        events = setOf(PASSED, SKIPPED, FAILED)
+    }
+    useJUnitPlatform()
 }
