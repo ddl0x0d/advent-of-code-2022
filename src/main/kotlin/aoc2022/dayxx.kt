66492f963569aa12967372e93cca23ed3176941f
Dmitrijs Laptevs <dmitry.laptev.0x0d@gmail.com>
2022-12-04 07:38:28 +0200

    Add puzzle template

---
+++ src/main/kotlin/aoc2022/dayxx.kt
@@ -0,0 +1,22 @@
+package aoc2022
+
+/**
+ * [Day XX: ???](https://adventofcode.com/2022/day/XX)
+ */
+@Suppress("unused")
+object DayXX : Puzzle<List<String>, Int> {
+
+    override val name = "???"
+
+    override fun parseInput(lines: List<String>): List<String> {
+        return lines
+    }
+
+    override fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    override fun part2(input: List<String>): Int {
+        return 0
+    }
+}
